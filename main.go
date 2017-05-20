83b9d3f94212db0844f65c90ef2df286b6776b54
Evan Lin <evan.linjin@gmail.com>
2017-05-20 17:34:32 +1200

    Done.

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"flag"
+	"golang.org/x/net/html"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	fStr := flag.String("f", "test.html", "file name")
+	flag.Parse()
+
+	f, e := os.Open(*fStr)
+	if e != nil {
+		log.Println(e)
+		return
+	}
+
+	var name, email string
+
+	z := html.NewTokenizer(f)
+	for {
+		switch z.Next() {
+		case html.StartTagToken:
+			t := z.Token()
+
+			if t.Data != "span" {
+				continue
+			}
+
+			var yes bool
+			yes, name, email = AttrsRepresentGD(t.Attr)
+			if yes {
+				goto finish
+			}
+
+		case html.ErrorToken:
+			return
+		}
+	}
+
+finish:
+	fmt.Println("SENDER EMAIL:", email)
+	fmt.Println("SENDER NAME:", name)
+}
+
+func AttrsRepresentGD(attrs []html.Attribute) (yes bool, name, email string) {
+	for _, a := range attrs {
+		switch a.Key {
+		case "class":
+			if a.Val != "gD" {
+				return
+			}
+			yes = true
+		case "email":
+			email = a.Val
+		case "name":
+			name = a.Val
+		}
+	}
+	return
 }
